e57b7a992e4f7cccbc007aa41cd8513a515d1d1e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-01 22:21:55 +0530

    initial commit for Wikimedia project

---
+++ kafka_service_1/src/main/java/com/kafka/kafka_service_1/KafkaService1Application.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka_service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaService1Application.class, args);
+	}
+
+}
